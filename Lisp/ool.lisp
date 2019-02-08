8c27a538833e7b2b09bd152b5ba1fbcf5a959c7c
MrTbolt <piovanid@gmail.com>
2019-02-08 20:28:35 +0100

    Update ool.lisp

--- Lisp/ool.lisp
+++ Lisp/ool.lisp
@@ -44,3 +44,11 @@
 ; spoiler, si, quello è il problema
 (defun new1(superC param)
 	(append (new (car superC) param) new1(cdr superC)))
+
+;funzione che dato un nome di una funzione e il suo corpo la definisce a tempo di esecuzione e ci aggiunge il parametro this
+;la funzione (nel senso di scopo) del parametro this è la seguente, al parametro this si passa come valore l'oggetto su cui si vuole
+;chiamare il metodo, e nella funzione i riferimenti a this passeranno al oggetto passato, di conseguenza ogni funzione oltre ai suoi parametri
+;avrà un parametro this messo appositamente per effettuare il collegamento tra l'utilizzo di this stesso e l'effettivo oggetto
+;piccola e potente, come il mio.. no aspet volevo dire come il mio google home mini! adoro il mio google home mini...
+(defun def-unction (name function) 
+	(compile (eval (append (list 'defun) (list name) (append (list (append (list 'this) (car function))) (cdr function))))))

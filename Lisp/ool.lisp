bb174eb84a5d89ea3d6cc102d83b2c13c07d1fe7
Michele Pugno <doc5.129@gmail.com>
2019-02-09 15:12:37 +0100

    Def class definita, inizio lavoro su new

--- Lisp/ool.lisp
+++ Lisp/ool.lisp
@@ -1,46 +1,92 @@
-;Primitiva def class hash table con tutte le definizioni di classi
-(defparameter classes (make-hash-table))
-
-;aggiunge classe alla hash table
+(defparameter *classes-specs* (make-hash-table))
 (defun add-class-spec (name class-spec)
-  (setf (gethash name classes) class-spec))
-
-;ritorna la struttura di una classe dato il nome della stessa
+  (setf (gethash name *classes-specs*) class-spec))
 (defun get-class-spec (name)
-  (gethash name classes))
+  (gethash name *classes-specs*))
 
-
+;Primitiva definizione classe, TODO per segnalare errore ritornare NIL
 (defun def-class (class-name parents &rest slot-value)
-  (if (null parents)
-    ))
+  (cond
+    ((not (symbolp class-name))
+    (error "Error: nome classe non e' un simbolo"))
+    
+    ((not (listp parents))
+    (error "Error: parent non e' una lista"))
+    
+    ((parents-control class-name parents)
+     (error "Error: classe e parents con stesso nome")
+     )
+    ((values-control (formatta slot-value))
+     (error "Error: bad values formatting")))
+  
+  (let*
+      ((genitori
+	(if (null parents)
+	    '(orfano)
+	    parents)))
+    (remhash class-name *classes-specs*) ;rimuovo classe se presente
+    (add-class-spec class-name (list genitori  (formatta slot-value))))
+  ;ritorno class-name da specifica
+  ;class-name
+  )
 
-;Primitiva New
-(defun new (class-name )
-  ())
+;Primitiva New 
+(defun new (class-name &rest parameters)
+  (if (values-control (formatta parameters))
+      (error "Error: bad format"))
+  
+  (if (and (get-class-spec class-name)
+	   (instance-check (first get-class-spec class-name) (formatta parameters)))
+      ('oolist class-name (formatta parameters));TODO da quotare?
+      NIL))
 
 ;Primitiva getv
 (defun getv (instance slot-name)
+  (let* ((is-in-instance (recursive-getv-instance (rest (rest instance)) slot-name)))
+    
+    (if (is-in-instance)
+	is-in-instance
+        (recursive-getv-tree (append (second instance) (first (get-class-spec class))) slot-name))))
+
+(defun recursive-getv-instance (values slot-name)
+  (if (equals values NIL)
+      NIL ;base
+      (or ;passo
+       (and (equals (first (first values)))
+	    (not (equals (second (first values) '=>))))) ;TODO controllare futura codifica metodi
+      (recursive-getv-instance (rest values) slot-name)
+      ))
+
+(defun recursive-getv-tree (classes slot-name)
   ())
 
 ;Primitiva getvx. Slot-name deve essere una lista non vuota
 (defun getvx (instance &rest slot-name)
   ())
 
- (defun formatta (slot-value)
+
+
+(defun formatta (slot-value)
 	(if (null slot-value) nil
 	(append (list (list (first slot-value) (second slot-value))) (formatta (cdr( cdr slot-value))))))
 
+;controllo che first sia simbolo in slot-value
+(defun values-control (values)
+  (if (equal values NIL)
+      NIL
+      (or (not(symbolp (first (first values))))
+	  (values-control (rest values)))))
 
-;def-class ora funziona
-(defun def-class (name parents &rest slot-value)
-	(add-class-spec  name (append (list parents) (formatta slot-value))))
+;controllo che parents non contenga il nome di classe
+(defun parents-control (class-name parents)
+  (if (equal parents NIL)
+      NIL
+      (or (equal (first parents) class-name)
+	  (parents-control class-name (rest parents)))))
 
-;new class not tested
-(defun new (class-name &rest param)
-	((if (null param) nil
-	 (append (new1 (car (get-class-spec class-name)) param) (cdr (get-class-spec class-name))))))
-;function that is used to cicle to the superclasses of a class and call the new recurvivly on them
-;TODO il problema potrebbe essere che chiamo passando param invece che un elenco di parametri &rest riceve una lista e quindi fa una lista che contiene la lista
-; spoiler, si, quello è il problema
-(defun new1(superC param)
-	(append (new (car superC) param) new1(cdr superC)))
+(defun instance-check (class parameters)
+  (if (equal parameters NIL)
+      T
+      (and (getv (list 'oolist class NIL) (first(first parameters))) ;TODO parametro potrebbe avere NIL come valore
+	   instance-check (class (rest parameters)))))
+

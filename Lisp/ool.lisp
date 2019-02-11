c81677f290e87768ba261b043a69974721c414ce
Michele Pugno <doc5.129@gmail.com>
2019-02-11 22:50:49 +0100

    Primitiva getvx funzionante (giocaci un po'). Primitive getv, def-class e new testate. Algoritmo di percorrenza della gerarchia di superclassi come da specifica. TODO: 	1) studiare implementazione dei metodi come richiesta dal docente. 	2) studiare le funzioni che il docente consiglia per i metodi, vedi foglio di specifica. 	3) capire le implicazioni che l'introduzione di liste (contenenti la definizione dei metodi) nel cdr delle tuple della class-spec hanno sulle primitive, queste devono comportarsi diversamente se si imbattono in un metodo? 	4) capire cosa succede se faccio il getv di un metodo (ad esempio) 	5) capire dove implementare la eval-compile del metodo, a definizione di classe o di istanza? il docente non pone preferenze.

--- Lisp/ool.lisp
+++ Lisp/ool.lisp
@@ -25,28 +25,28 @@
 	    NIL
 	    parents)))
     (remhash class-name *classes-specs*) ;rimuovo classe se presente
-    (add-class-spec class-name (list genitori  (formatta slot-value))))
+    (add-class-spec class-name (list genitori (formatta slot-value))))
   ;ritorno class-name da specifica
-  ;class-name
-  )
+  class-name)
 
-;controllo che first sia simbolo in slot-value
+;controllo che first sia simbolo in slot-value formattata
 (defun values-control (values)
   (if (equal values NIL)
       NIL
       (or (not(symbolp (first (first values))))
 	  (values-control (rest values)))))
 
-;controllo che parents non contenga il nome di classe
+;controllo esistenza parents e omonimia con class-name
 (defun parents-control (class-name parents)
-  (cond ((equal parents NIL)
-	 NIL)
+  (cond
+    ((equal parents NIL)
+     NIL)	
+
+    ((not (get-class-spec (first parents)))
+     (error "Errore: classe genitore non esistente"))
 	
-	((not (get-class-spec (first parents)))
-	 (error "Errore: classe genitore non esistente")) ;controllo che esistano i genitori
-	
-	((or (equal (first parents) class-name)
-	     (parents-control class-name (rest parents)))))) ;controllo uguaglianza
+    ((or (equal (first parents) class-name)
+	 (parents-control class-name (rest parents))))))
 
 ;Primitiva New 
 (defun new (class-name &rest parameters)
@@ -61,20 +61,25 @@
 (defun instance-check (class parameters)
   (if (equal parameters NIL)
       T
-      (and (getv (list 'oolinst class NIL) (first(first parameters))) ;TODO parametro potrebbe avere NIL, chiamo errore
+      (and (getv (list 'oolinst class NIL) (first(first parameters)))
 	   (instance-check class (rest parameters)))))
 
 ;Primitiva getv
 (defun getv (instance slot-name)
-  (if (not slot-name)
-      (error "Errore: inserire un parametro"))
-  (if (not (equal 'oolinst (first instance)))
-      (error "Errore: istanza non valida"))
-  
-  (let* ((is-in-instance (recursive-getv-instance (third instance) slot-name))) ; corretto errore
+  (cond
+    ((or (not (symbolp slot-name))
+	 (null slot-name))
+     (error "Errore: slot-name non valido"))
+    
+    ((not (equal 'oolinst (first instance)))
+     (error "Errore: istanza non valida")))
+
+  (let* ((is-in-instance
+	  (recursive-getv-instance (third instance) slot-name)))
     (if is-in-instance
 	(second is-in-instance)
-	(let* ((is-in-tree (recursive-getv-tree (list (second instance)) slot-name))); uso tree2
+	(let* ((is-in-tree
+		(recursive-getv-tree (list (second instance)) slot-name)))
 	  (if is-in-tree
 	      (second is-in-tree)
 	      (error "Errore: valore non valido"))))))
@@ -86,14 +91,14 @@
     
     ((and (equal (first (first values)) slot-name)
 	  (not (equal (second (first values)) '=>)))
-     (first values)) ;TODO controllare futura codifica metodi
+     (first values)) ;TODO controllare futura codifica metodi TODO
     
     ((not (equal (first (first values)) slot-name))
      (recursive-getv-instance (rest values) slot-name)))) ;passo
 
 (defun recursive-getv-tree (classes slot-name) ; ritorna una coppia ;FINISHED
-  (let* ((is-in-level (recursive-getv-instance (second (get-class-spec (first classes)))
-					       slot-name)))
+  (let* ((is-in-level
+	  (recursive-getv-instance (second (get-class-spec (first slot-name)))
     (cond
       ((equal classes  NIL)
        NIL)
@@ -102,35 +107,38 @@
        is-in-level)
       
       ((not is-in-level)
-       (recursive-getv-tree (append (first (get-class-spec (first classes))) (rest classes))
-			     slot-name)))))
+       (recursive-getv-tree (append (first (get-class-spec (first classes)))
+				    (rest classes))
+			    slot-name)))))
 
-;Primitiva getvx. Slot-name deve essere una lista non vuota. divisione in 2 metodi per 2 motivi 
-;-primo il controllo che slot-name sia ugugale a nil non deve interferire con lo stesso controllo fatto nella ricorsione
-;-secondo  perchè altrimenti no va prorpio, &rest e ricorsione non vanno daccordo come ti avevo detto
-(defun getvx (instance &rest slot-name) (if (null slot-name)
-                                            (error "Error: non sono stati passati slot-name")
-                                            (getvx1 instance slot-name)))
+;Primitiva getvx
+(defun getvx (instance &rest slot-name)
+  (cond
+    ((null slot-name)
+     (error "Errore: slot-name vuoto"))
+    
+    ((not (equal 'oolinst (first instance)))
+     (error "Errore: istanza non valida")))
+  (getvx-recursive instance slot-name))
 
-(defun getvx1 (instance slot-name)  (if (null slot-name)
-                                        instance
-                                        (getv (getvx instance (butlast slot-name)) (last slot-name))))
+(defun getvx-recursive (instance slot-name)
+  (cond
+    ((not (symbolp (first slot-name)))
+     (error "Errore: slot-names devono essere simboli"))
+    
+    ((null (rest slot-name))
+     (getv instance (first slot-name)))
+
+    ((rest slot-name)
+     (getvx-recursive (getv instance (first slot-name)) (rest slot-name)))))
 
 ;funzione formattazione tuple
 (defun formatta (slot-value)
-	(if (null slot-value) nil
-	(append (list (list (first slot-value) (second slot-value))) (formatta (cdr( cdr slot-value))))))
+  (if (null slot-value)
+      nil
+      (append (list (list (first slot-value) (second slot-value)))
+	      (formatta (cdr( cdr slot-value))))))
 
-;new class not tested
-;(defun new (class-name &rest param)
-;	((if (null param) nil
-;	 (append (new1 (car (get-class-spec class-name)) param) (cdr (get-class-spec class-name))))))
-;function that is used to cicle to the superclasses of a class and call the new recurvivly on them
-;TODO il problema potrebbe essere che chiamo passando param invece che un elenco di parametri &rest riceve una lista e quindi fa una lista che contiene la lista
-; spoiler, si, quello è il problema
-;(defun new1(superC param)
-;	(append (new (car superC) param) new1(cdr superC)))
-;
 ;funzione che dato un nome di una funzione e il suo corpo la definisce a tempo di esecuzione e ci aggiunge il parametro this
 ;la funzione (nel senso di scopo) del parametro this è la seguente, al parametro this si passa come valore l'oggetto su cui si vuole
 ;chiamare il metodo, e nella funzione i riferimenti a this passeranno al oggetto passato, di conseguenza ogni funzione oltre ai suoi parametri

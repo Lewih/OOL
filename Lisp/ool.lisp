7b0844c18fe5a75e4ce6231e956804fcec042b8b
Michele Pugno <doc5.129@gmail.com>
2019-02-14 23:07:22 +0100

    Beautify, methods skeleton

--- Lisp/ool.lisp
+++ Lisp/ool.lisp
@@ -4,18 +4,22 @@
 (defun get-class-spec (name)
   (gethash name *classes-specs*))
 
-;Primitiva definizione classe, TODO per segnalare errore ritornare NIL
+;funzione formattazione per lista associativa
+(defun formatta (slot-value)
+  (if (null slot-value)
+      nil
+      (append (list (list (first slot-value) (second slot-value)))
+	      (formatta (cdr( cdr slot-value))))))
+
+;Primitiva definizione classe
 (defun def-class (class-name parents &rest slot-value)
   (cond
     ((not (symbolp class-name))
      (error "Error: nome classe non e' un simbolo"))
-    
     ((not (listp parents))
      (error "Error: parent non e' una lista"))
-    
     ((parents-control class-name parents)
      (error "Error: classe e parents con stesso nome"))
-    
     ((values-control (formatta slot-value))
      (error "Error: bad values formatting")))
   
@@ -40,11 +44,9 @@
 (defun parents-control (class-name parents)
   (cond
     ((equal parents NIL)
-     NIL)	
-
+     NIL)
     ((not (get-class-spec (first parents)))
      (error "Errore: classe genitore non esistente"))
-	
     ((or (equal (first parents) class-name)
 	 (parents-control class-name (rest parents))))))
 
@@ -52,7 +54,6 @@
 (defun new (class-name &rest parameters)
   (if (values-control (formatta parameters))
       (error "Error: bad format"))
-  
   (if (and (get-class-spec class-name)
 	   (instance-check class-name (formatta parameters)))
       (list 'oolinst class-name (formatta parameters))
@@ -70,10 +71,9 @@
     ((or (not (symbolp slot-name))
 	 (null slot-name))
      (error "Errore: slot-name non valido"))
-    
     ((not (equal 'oolinst (first instance)))
      (error "Errore: istanza non valida")))
-
+  
   (let* ((is-in-instance
 	  (recursive-getv-instance (third instance) slot-name)))
     (if is-in-instance
@@ -88,10 +88,8 @@
   (cond
     ((equal values NIL)
      NIL);base
-    
     ((equal (first (first values)) slot-name)
      (first values)) ;tratto i metodi come valori
-    
     ((not (equal (first (first values)) slot-name))
      (recursive-getv-instance (rest values) slot-name)))) ;passo
 
@@ -102,10 +100,8 @@
     (cond
       ((equal classes  NIL)
        NIL)
-      
       (is-in-level
        is-in-level)
-      
       ((not is-in-level)
        (recursive-getv-tree (append (first (get-class-spec (first classes)))
 				    (rest classes))
@@ -115,32 +111,33 @@
 (defun getvx (instance &rest slot-name)
   (if (null slot-name)
       (error "Errore: slot-name vuoto"))
-  
   (getvx-recursive instance slot-name))
 
 (defun getvx-recursive (instance slot-name)
   (cond
     ((not (symbolp (first slot-name)))
      (error "Errore: slot-names devono essere simboli"))
-    
     ((null (rest slot-name))
      (getv instance (first slot-name)))
-
     ((rest slot-name)
      (getvx-recursive (getv instance (first slot-name)) (rest slot-name)))))
 
-;funzione formattazione tuple
-(defun formatta (slot-value)
-  (if (null slot-value)
-      nil
-      (append (list (list (first slot-value) (second slot-value)))
-	      (formatta (cdr( cdr slot-value))))))
+
+;Metodi, definisco il metodo in chiamata new
+
+;riscrivo S-expression cosi da poter usare this
+(defun rewrite-method-code (method-name method-spec))
+
+;funzione principale
+(defun process-method (method-name method-spec)
+  (eval (rewrite-method-code method-name method-spec)))
+
 
 ;funzione che dato un nome di una funzione e il suo corpo la definisce a tempo di esecuzione e ci aggiunge il parametro this
-;la funzione (nel senso di scopo) del parametro this è la seguente, al parametro this si passa come valore l'oggetto su cui si vuole
+;la funzione (nel senso di scopo) del parametro this e la seguente, al parametro this si passa come valore l'oggetto su cui si vuole
 ;chiamare il metodo, e nella funzione i riferimenti a this passeranno al oggetto passato, di conseguenza ogni funzione oltre ai suoi parametri
-;avrà un parametro this messo appositamente per effettuare il collegamento tra l'utilizzo di this stesso e l'effettivo oggetto
-;piccola e potente, come il mio.. no aspet volevo dire come il mio google home mini! adoro il mio google home mini...
+;avra un parametro this messo appositamente per effettuare il collegamento tra l'utilizzo di this stesso e l'effettivo oggetto
+
 (defun def-unction (name function) 
   (compile (eval (append (list 'defun) (list name) (append (list (append (list 'this) (car function)))
 							   (cdr function))))))

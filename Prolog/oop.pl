4a4175affd57636ef40dbec690d288a86b39916e
MrTbolt <piovanid@gmail.com>
2019-02-22 10:33:22 +0100

    Update oop.pl

--- Prolog/oop.pl
+++ Prolog/oop.pl
@@ -36,3 +36,21 @@ getv(Instance, Slot, Result) :-
     instance(Instance, Parents, Values),
     
     !.
+
+%versione migliore ma che tiene l'ultima occorrenza	
+%rimuovi_duplicati([], []).
+%rimuovi_duplicati([First | Rest], NewRest) :- member(First, Rest), rimuovi_duplicati(Rest, NewRest).
+%rimuovi_duplicati([First | Rest], [First | NewRest]) :- not(member(First, Rest)), rimuovi_duplicati(Rest, NewRest).
+	
+	
+not_member(X,[]).
+not_member(X,[X|T]):- fail.
+not_member(X,[Y|T]):- not_member(X,T).
+	
+%versione corretta ma che usa not, non so per quale motivo non riesco a far andare la not member lol
+rimuovi_duplicati(A,B) :- rimuovi_duplicati(A, B, []).
+rimuovi_duplicati([],[],_).
+rimuovi_duplicati([H|T],[H|Out],Old) :- not(member(H,Old)), rimuovi_duplicati(T,Out, [H|Old]).
+rimuovi_duplicati([H|T],Out, Old) :- member(H,Old), rimuovi_duplicati(T,Out,Old).
+	
+

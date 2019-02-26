06a5cfd82012b85938e5165ca5acbd1ea589f0d8
Michele Pugno <doc5.129@gmail.com>
2019-02-26 15:11:49 +0100

    Beautify, aggiunta cut nei metodi , aggiunta variante in prepare args nel caso in cui l-unico argomento sia l'istanza stessa

--- Prolog/oop.pl
+++ Prolog/oop.pl
@@ -42,7 +42,8 @@ new(Instance, Class_name, Values) :-
     append(Out_clean, [_], Out),
     find_method(Out_clean, Instance),!.
 
-class_values(_,[]).
+class_values(_, []).
+
 class_values(Class, [Name = _|Others]) :-
     getv_hierarchy([Class], Name, Value),
     class_values(Class, Others).
@@ -63,8 +64,6 @@ get_all(Instance, Slot, Result) :-
     instance(Instance, Classname, _),
     getv_hierarchy([Classname], Slot, Result).
 
-
-
 getv_hierarchy([], _, _).
 
 getv_hierarchy([Class|_], Slot, Result) :-
@@ -75,11 +74,6 @@ getv_hierarchy([Class|Parents], Slot, Result) :-
     class(Class, New_parents, _),
     append(New_parents, Parents, New_list),
     getv_hierarchy(New_list, Slot, Result).
-	
-value_in_list([Name = Value|_], Name, Value).
-
-value_in_list([_ = _|Tail],Name,Result) :-
-    value_in_list(Tail,Name,Result).
 
 getvx(Instance, [Slot], Result) :-
     getv(Instance, Slot, Result).
@@ -88,23 +82,31 @@ getvx(Instance, [Slot|Others], Result) :-
     getv(Instance, Slot, Match),
     getvx(Match, Others, Result).
 
+value_in_list([Name = Value|_], Name, Value).
 
-	
-not_member(X,[]).
-not_member(X,[Y|T]):-
-    X\=Y, not_member(X,T).
+value_in_list([_ = _|Tail],Name,Result) :-
+    value_in_list(Tail, Name, Result).
 
-rimuovi_duplicati([],[],_).
-rimuovi_duplicati(A,B) :-
+not_member(X, []).
+
+not_member(X, [Y|T]) :-
+    X \= Y,
+    not_member(X, T).
+
+rimuovi_duplicati([], [],_).
+
+rimuovi_duplicati(A, B) :-
     rimuovi_duplicati(A, B, []).
-rimuovi_duplicati([H|T],[H|Out],Old) :-
-    not_member(H,Old),
-    rimuovi_duplicati(T,Out, [H|Old]).
-rimuovi_duplicati([H|T],Out, Old) :-
-    member(H,Old),
-    rimuovi_duplicati(T,Out,Old).
 
-prepare_method(Name, Method_term, Output):-
+rimuovi_duplicati([H|T], [H|Out], Old) :-
+    not_member(H, Old),
+    rimuovi_duplicati(T, Out, [H|Old]).
+
+rimuovi_duplicati([H|T], Out, Old) :-
+    member(H, Old),
+    rimuovi_duplicati(T, Out, Old).
+
+prepare_method(Name, Method_term, Output) :-
     atom_string(Name, Name_string),
     term_string(Method_term, Method),
     sub_string(Method, Before, Length, End, "this"),
@@ -116,26 +118,38 @@ prepare_method(Name, Method_term, Output):-
     string_concat(Name_string, Out, Output1),
     string_concat(Start, Output1, Output2),
     prepare_method(Name, Output2, Output).
-prepare_method(Name, Meth, Out):-
+
+prepare_method(Name, Meth, Out) :-
     string(Meth),
-    Out=Meth,
+    Out = Meth,
     !.
 
-prepare_method(Name, Meth, Out):-
+prepare_method(Name, Meth, Out) :-
     term_string(Meth, Out).
+
+prepare_args(Name, Args, Result) :-
+    term_string(Args, String),
+    sub_string(String, 1, _, 1, String_clean),
+    String_clean \= "",
+    atom_string(Name, Name_string),
+    string_concat("( ", Name_string, Out),
+    string_concat(Out, ", ", Out2),
+    string_concat(Out2, String_clean, Out3),
+    string_concat(Out3, ")", Result),
+    !.
 
 prepare_args(Name, Args, Result) :-
     term_string(Args, String),
     sub_string(String, 1, _, 1, String_clean),
     atom_string(Name, Name_string),
     string_concat("( ", Name_string, Out),
-    string_concat(Out, ", ", Out2),
-    string_concat(Out2, String_clean, Out3),
-    string_concat(Out3, ")", Result).
-
+    string_concat(Out, String_clean, Out2),
+    string_concat(Out2, ")", Result),
+    !.
     
+find_method([], _) :-
+    !.
 
-find_method([], _):-!.
 find_method([Head|Tail], Instance) :-
     nth0(0, Head, Name),
     nth0(1, Head, method(Args, Body)),
@@ -143,7 +157,8 @@ find_method([Head|Tail], Instance) :-
     find_method(Tail, Instance).
 
 find_method([Head|Tail], Instance) :-
-    find_method(Tail, Instance),!.
+    find_method(Tail, Instance),
+    !.
 
 define_method(Name = method(Args, Body), Instance) :-
     prepare_args(Instance, Args, New_args),
@@ -151,6 +166,7 @@ define_method(Name = method(Args, Body), Instance) :-
     atom_string(Name, Name_out),
     string_concat(Name_out, New_args, Out),
     string_concat(Out, ":-", Out2),
-    string_concat(Out2, Body_out, Result), %TODO aggiungere cut finale al metodo
+    string_concat(Out2, Body_out, Out3),
+    string_concat(Out3, ",!.", Result),
     term_string(Term, Result),
     assert(Term).
